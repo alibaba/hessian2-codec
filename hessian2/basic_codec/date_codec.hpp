7631ed04bac9e3a6931682c1e94515afe9b6cace
code <wbpcode@users.noreply.github.com>
2021-02-03 17:31:16 +0800

    Move template functions definition in basic_codec to independent .cc file  (#8)

--- hessian2/basic_codec/date_codec.hpp
+++ hessian2/basic_codec/date_codec.hpp
@@ -53,101 +53,56 @@ void writeDate(
 
 }  // namespace
 
-// # time in UTC encoded as 64-bit long milliseconds since epoch
-// ::= x4a b7 b6 b5 b4 b3 b2 b1 b0
-// ::= x4b b3 b2 b1 b0       # minutes since epoch
 template <>
-std::unique_ptr<std::chrono::milliseconds> Decoder::decode() {
-  return readDate<std::chrono::milliseconds>(reader_);
-}
+std::unique_ptr<std::chrono::milliseconds> Decoder::decode();
 
 template <>
-std::unique_ptr<std::chrono::minutes> Decoder::decode() {
-  return readDate<std::chrono::minutes>(reader_);
-}
+std::unique_ptr<std::chrono::minutes> Decoder::decode();
 
 template <>
-std::unique_ptr<std::chrono::seconds> Decoder::decode() {
-  return readDate<std::chrono::seconds>(reader_);
-}
+std::unique_ptr<std::chrono::seconds> Decoder::decode();
 
 template <>
-std::unique_ptr<std::chrono::hours> Decoder::decode() {
-  return readDate<std::chrono::hours>(reader_);
-}
+std::unique_ptr<std::chrono::hours> Decoder::decode();
 
 #if _LIBCPP_STD_VER > 17
 template <>
-std::unique_ptr<std::chrono::days> Decoder::decode() {
-  return readDate<std::chrono::days>(reader_);
-}
-std::unique_ptr<std::chrono::weeks> Decoder::decode() {
-  return readDate<std::chrono::weeks>(reader_);
-}
-std::unique_ptr<std::chrono::years> Decoder::decode() {
-  return readDate<std::chrono::years>(reader_);
-}
-std::unique_ptr<std::chrono::months> Decoder::decode() {
-  return readDate<std::chrono::months>(reader_);
-}
+std::unique_ptr<std::chrono::days> Decoder::decode();
+
+template <>
+std::unique_ptr<std::chrono::weeks> Decoder::decode();
+
+template <>
+std::unique_ptr<std::chrono::years> Decoder::decode();
+
+template <>
+std::unique_ptr<std::chrono::months> Decoder::decode();
 #endif
 
-// # time in UTC encoded as 64-bit long milliseconds since epoch
-// ::= x4a b7 b6 b5 b4 b3 b2 b1 b0
-// ::= x4b b3 b2 b1 b0       # minutes since epoch
+template <>
+bool Encoder::encode(const std::chrono::minutes &value);
 
 template <>
-bool Encoder::encode(const std::chrono::minutes &value) {
-  writer_->writeByte(0x4b);
-  writer_->writeBE<int32_t>(value.count());
-  return true;
-}
+bool Encoder::encode(const std::chrono::milliseconds &value);
 
 template <>
-bool Encoder::encode(const std::chrono::milliseconds &value) {
-  std::chrono::minutes value_min =
-      std::chrono::duration_cast<std::chrono::minutes>(value);
-  if (value_min.count() * 60000 == value.count()) {
-    return encode<std::chrono::minutes>(value_min);
-  }
-  writer_->writeByte(0x4a);
-  writer_->writeBE<int64_t>(value.count());
-  return true;
-}
+bool Encoder::encode(const std::chrono::seconds &value);
 
 template <>
-bool Encoder::encode(const std::chrono::seconds &value) {
-  writeDate<std::chrono::seconds>(writer_, value);
-  return true;
-}
-
-template <>
-bool Encoder::encode(const std::chrono::hours &value) {
-  writeDate<std::chrono::hours>(writer_, value);
-  return true;
-}
+bool Encoder::encode(const std::chrono::hours &value);
 
 #if _LIBCPP_STD_VER > 17
 template <>
-bool Encoder::encode(const std::chrono::days &value) {
-  writeDate<std::chrono::days>(writer_, value);
-  return true;
-}
+bool Encoder::encode(const std::chrono::days &value);
+
 template <>
-bool Encoder::encode(const std::chrono::weeks &value) {
-  writeDate<std::chrono::weeks>(writer_, value);
-  return true;
-}
+bool Encoder::encode(const std::chrono::weeks &value);
+
 template <>
-bool Encoder::encode(const std::chrono::years &value) {
-  writeDate<std::chrono::years>(writer_, value);
-  return true;
-}
+bool Encoder::encode(const std::chrono::years &value);
+
 template <>
-bool Encoder::encode(const std::chrono::months &value) {
-  writeDate<std::chrono::months>(writer_, value);
-  return true;
-}
+bool Encoder::encode(const std::chrono::months &value);
 #endif
 
 }  // namespace Hessian2
--- hessian2/basic_codec/ref_object_codec.hpp
+++ hessian2/basic_codec/ref_object_codec.hpp
@@ -6,31 +6,10 @@
 
 namespace Hessian2 {
 
-// ref ::= x51 int
 template <>
-std::unique_ptr<RefObject> Decoder::decode() {
-  auto ret = reader_->read<uint8_t>();
-  ABSL_ASSERT(ret.first);
-  ABSL_ASSERT(ret.second == 0x51);
-  auto ref = decode<int32_t>();
-  if (!ref) {
-    return nullptr;
-  }
-  if (static_cast<uint32_t>(*ref) >= values_ref_.size()) {
-    return nullptr;
-  }
-  return std::make_unique<RefObject>(values_ref_[*ref]);
-}
+std::unique_ptr<RefObject> Decoder::decode();
 
 template <>
-bool Encoder::encode(const RefObject& value) {
-  auto d = value.toRefDest();
-  ABSL_ASSERT(d.has_value());
-  auto ref = getValueRef(d.value());
-  ABSL_ASSERT(ref != -1);
-  writer_->writeByte<uint8_t>(0x51);
-  encode<int32_t>(ref);
-  return true;
-}
+bool Encoder::encode(const RefObject& value);
 
 }  // namespace Hessian2
