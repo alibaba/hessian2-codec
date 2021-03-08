4ef3e890680dabb9cc1bddef0969a2b560566664
wbpcode <comems@msn.com>
2021-03-08 20:54:14 +0800

    fix data encoding/decoding for windows
    Signed-off-by: wbpcode <comems@msn.com>

--- hessian2/basic_codec/date_codec.cc
+++ hessian2/basic_codec/date_codec.cc
@@ -2,6 +2,46 @@
 
 namespace Hessian2 {
 
+namespace {
+
+template <typename T>
+std::unique_ptr<T> readDate(ReaderPtr &reader) {
+  auto out = std::make_unique<T>();
+  uint8_t code = reader->read<uint8_t>().second;
+  switch (code) {
+    case 0x4b:
+      if (reader->byteAvailable() < 4) {
+        return nullptr;
+      }
+      return std::make_unique<T>(std::chrono::duration_cast<T>(
+          std::chrono::minutes(reader->readBE<int32_t>().second)));
+    case 0x4a:
+      if (reader->byteAvailable() < 8) {
+        return nullptr;
+      }
+      return std::make_unique<T>(std::chrono::duration_cast<T>(
+          std::chrono::milliseconds(reader->readBE<int64_t>().second)));
+  }
+  return nullptr;
+}
+
+template <typename T>
+void writeDate(WriterPtr &writer, const T &value) {
+  int64_t value_in_min =
+      std::chrono::duration_cast<std::chrono::minutes>(value).count();
+  int64_t value_in_ms =
+      std::chrono::duration_cast<std::chrono::milliseconds>(value).count();
+  if (value_in_min * 60000 == value_in_ms) {
+    writer->writeByte(0x4b);
+    writer->writeBE<int32_t>(value_in_min);
+  } else {
+    writer->writeByte(0x4a);
+    writer->writeBE<int64_t>(value_in_ms);
+  }
+}
+
+}  // namespace
+
 // # time in UTC encoded as 64-bit long milliseconds since epoch
 // ::= x4a b7 b6 b5 b4 b3 b2 b1 b0
 // ::= x4b b3 b2 b1 b0       # minutes since epoch
--- hessian2/basic_codec/date_codec.hpp
+++ hessian2/basic_codec/date_codec.hpp
@@ -8,51 +8,6 @@
 
 namespace Hessian2 {
 
-namespace {
-
-template <typename T>
-std::unique_ptr<
-    typename std::enable_if<std::chrono::__is_duration<T>::value, T>::type>
-readDate(ReaderPtr &reader) {
-  auto out = std::make_unique<T>();
-  uint8_t code = reader->read<uint8_t>().second;
-  switch (code) {
-    case 0x4b:
-      if (reader->byteAvailable() < 4) {
-        return nullptr;
-      }
-      return std::make_unique<T>(std::chrono::duration_cast<T>(
-          std::chrono::minutes(reader->readBE<int32_t>().second)));
-    case 0x4a:
-      if (reader->byteAvailable() < 8) {
-        return nullptr;
-      }
-      return std::make_unique<T>(std::chrono::duration_cast<T>(
-          std::chrono::milliseconds(reader->readBE<int64_t>().second)));
-  }
-  return nullptr;
-}
-
-template <typename T>
-void writeDate(
-    WriterPtr &writer,
-    const typename std::enable_if<std::chrono::__is_duration<T>::value, T>::type
-        &value) {
-  int64_t value_in_min =
-      std::chrono::duration_cast<std::chrono::minutes>(value).count();
-  int64_t value_in_ms =
-      std::chrono::duration_cast<std::chrono::milliseconds>(value).count();
-  if (value_in_min * 60000 == value_in_ms) {
-    writer->writeByte(0x4b);
-    writer->writeBE<int32_t>(value_in_min);
-  } else {
-    writer->writeByte(0x4a);
-    writer->writeBE<int64_t>(value_in_ms);
-  }
-}
-
-}  // namespace
-
 template <>
 std::unique_ptr<std::chrono::milliseconds> Decoder::decode();
 
